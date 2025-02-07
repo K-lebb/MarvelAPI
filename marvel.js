9d8ddc6401b264bf6af63a542d7d8f6830baa943
kaleb <gabrielaragaosampaio@gmail.com>
2025-02-06 22:10:20 -0300

    Ajustei a paginação dos herois para permitir que mais herois possam ser carregados sem que o site fique pesado. Além disso, ajustei um estilo para os botões de navegações entre as paginações oferecidas

--- marvel.js
+++ marvel.js
@@ -2,20 +2,49 @@ const timeStamp = '1737300360';
 const apiKey = 'dd361ff2995c08961d504690af3e99ac';
 const md5 = 'b9167f19018d6d34a7fc5e130087008a';
 
+const prevButton = document.createElement("button");
+prevButton.innerText = "Anterior";
+prevButton.disabled = true;
+prevButton.classList.add("prev");
+
+const nextButton = document.createElement("button");
+nextButton.innerText = "Próximo";
+nextButton.classList.add("next");
+
+let fisrtPag = 0;
+const characterLimit = 15;
+
+function nextPage() {
+    fisrtPag += characterLimit;
+    fetchCharacters();
+}
+
+function prevPage() {
+    if (fisrtPag >= characterLimit) {
+        fisrtPag -= characterLimit;
+        fetchCharacters();
+    }
+}
+
+prevButton.addEventListener("click", prevPage);
+nextButton.addEventListener("click", nextPage);
+
 fetchCharacters();
 
 const searchButton = document.querySelector('#searchButton');
 searchButton.addEventListener('click', () => {
     const query = document.querySelector('#searchInput').value.trim();
-    fetchCharacters(query); 
+    fetchCharacters(query);
 });
 
 function fetchCharacters(query = '') {
-    const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${timeStamp}&apikey=${apiKey}&hash=${md5}&limit=100${query ? '&nameStartsWith=' + query : ''}`;
+    const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${timeStamp}&apikey=${apiKey}&hash=${md5}&limit=${characterLimit}&offset=${fisrtPag}${query ? '&nameStartsWith=' + query : ''}`;
+
 
     fetch(url)
         .then((response) => response.json())
         .then((jsonParsed) => {
+
             const divHero = document.querySelector('#herois');
             divHero.innerHTML = ''; 
 
@@ -24,6 +53,9 @@ function fetchCharacters(query = '') {
             }
 
             jsonParsed.data.results.forEach(element => {
+                prevButton.disabled = fisrtPag === 0;
+                nextButton.disabled = jsonParsed.data.results.length < characterLimit;
+
                 const srcImage = element.thumbnail.path + '.' + element.thumbnail.extension;
                 const nameHero = element.name;
 
@@ -46,10 +78,16 @@ function createDivHero(srcImage, nameHero, divToAppend) {
     textName.textContent = nameHero;
     img.src = srcImage;
 
-    divFilho.appendChild(img);
     divFilho.appendChild(textName);
+    divPai.appendChild(img);
     divPai.appendChild(divFilho);
     divToAppend.appendChild(divPai);
 
     divPai.classList.add('character-card');
 }
+
+const paginationContainer = document.createElement("div");
+paginationContainer.appendChild(prevButton);
+paginationContainer.appendChild(nextButton);
+paginationContainer.classList.add("pagination");
+document.body.appendChild(paginationContainer);
